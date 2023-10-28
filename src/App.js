1db790d64da5e632acdfb4fbcaea0df956116e60
NayyKo <djebarlen.tambon@gmail.com>
2023-10-28 10:32:10 +0400

    Changement de version de React Router Dom : Passage en version @6.10.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
 import { Single } from "./Pages/Single/Single";
 import Navbar from "./components/Navbar/Navbar";
 import PageError from "./PageError";
@@ -9,68 +9,88 @@ import TeamSOI from "./Pages/Team/Team";
 import Home from "./Pages/Home/Home";
 import Footer from './components/Footer/Footer';
 
-const router = createBrowserRouter([
-  {
-    element: <Root />,
-    children: [
-      {
-        path:'/',
-        element:<Home />,        
-      },
-      {
-        path:'/synergieoi',
-        element:<Home />,        
-      },
-      {
-        path:'/synergie',
-        element:<Home />,        
-      },
-      {
-        path:'/synoi',
-        element:<Home />,        
-      },
-      {
-        path:'actus',
-        element:<Single />,        
-      },
-      {
-        path:'nos-centres',
-        element:<NosCentres />,        
-      },
-      {
-        path:'nos-formations',
-        element:<NosFormations />,
-      },
-      {
-        path:'team',
-        element:<TeamSOI />,        
-      },
-      {
-        path:'contact',
-        element:<Contact />,        
-      },
-      
-    ],
-    errorElement:<PageError />,
-  }
-]);
 
-function Root(){
-  return (
-    <>
-      <Navbar />
-      <div className="">
-        <Outlet />
-      </div>
-      <Footer />
-    </>
-  )
-}
+
+
+
+// const router = createBrowserRouter([
+//   {
+//     element: <Root />,
+//     children: [
+//       {
+//         path:'/',
+//         element:<Home />,        
+//       },
+//       {
+//         path:'/synergieoi',
+//         element:<Home />,        
+//       },
+//       {
+//         path:'/synergie',
+//         element:<Home />,        
+//       },
+//       {
+//         path:'/synoi',
+//         element:<Home />,        
+//       },
+//       {
+//         path:'actus',
+//         element:<Single />,        
+//       },
+//       {
+//         path:'nos-centres',
+//         element:<NosCentres />,        
+//       },
+//       {
+//         path:'nos-formations',
+//         element:<NosFormations />,
+//       },
+//       {
+//         path:'team',
+//         element:<TeamSOI />,        
+//       },
+//       {
+//         path:'contact',
+//         element:<Contact />,        
+//       },
+      
+//     ],
+//     errorElement:<PageError />,
+//   }
+// ]);
+
+// function Root(){
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="">
+//         <Outlet />
+//       </div>
+//       <Footer />
+//     </>
+//   )
+// }
 
 export default function App() {
   return (
     <>
-      <RouterProvider router={router}/>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/synergieoi" element={<Home/> } />
+            <Route path="/synoi" element={<Home />} />
+            <Route path="/synergie" element={<Home />} />
+            <Route path="/actus" element={<Single />} />
+            <Route path="/nos-centres" element={<NosCentres />} />
+            <Route path="/nos-formations" element={<NosFormations />} />
+            <Route path="/team" element={<TeamSOI />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
     </>
   );
 }
